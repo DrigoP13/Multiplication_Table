6f039260a16e724d4066538c9f75e588899afe7d
Drigo <rodrigo.polastro03@gmail.com>
2021-04-30 21:09:24 -0300

    Creation of Index doc, start of css modifications

--- Js/script.js
+++ Js/script.js
@@ -4,15 +4,17 @@
 	let cont = 1
 	function tabuada(event){
 		event.preventDefault()
+		document.querySelector("table").style.background="red"
 		let mult = 1		
 		let number = document.getElementById('number').value
+					 document.getElementById('number').value = ''	
 		if (!number){
 			document.querySelector('.error').innerText = "Por favor, insira um número!"
 		} else {
 			document.querySelector('.error').innerText = "Tabuada Gerada:"
 			document.getElementsByTagName("th")[0].innerText = "Número"
 			document.getElementsByTagName("th")[1].innerText = "Multiplicador" 
-			document.getElementsByTagName("th")[2].innerText = "Resultado"
+			document.getElementsByTagName("th")[2].innerText = "Resultado" 
 			if (cont == 1){
 				for (i = 1; i <= 10; i++) {
 					let linha = document.createElement("tr")
@@ -23,6 +25,7 @@
 					let texto_numero = document.createTextNode(number)
 					let texto_mult = document.createTextNode("x "+mult)
 					let texto_result = document.createTextNode(number * mult)
+					console.log(mult,"if")
 					mult++
 					
 					coluna_numero.appendChild(texto_numero)
@@ -46,7 +49,9 @@
 					selector++
 					document.getElementsByTagName("td")[selector].innerText = (number * mult)
 					selector++
+					console.log(mult,"else")
 					mult++
+					
 				}
 			};	
 		};	
